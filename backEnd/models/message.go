bc03e18f19bd35547e94e1b091b3132c8a190079
Krishnasai-9959 <krishnasupervue.ai>
2026-05-28 16:46:21 +0530

    Small syntax errors fixed

--- backEnd/models/message.go
+++ backEnd/models/message.go
@@ -7,17 +7,11 @@ import (
 )
 
 type Message struct {
-	ID primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-
-	SenderID string `json:"sender_id" bson:"sender_id"`
-
-	ReceiverID string `json:"receiver_id" bson:"receiver_id"`
-
-	Content string `json:"content" bson:"content"`
-
-	Type string `json:"type" bson:"type"`
-
-	Status string `json:"status" bson:"status"`
-
-	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	ID         primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	SenderID   string             `json:"sender_id" bson:"sender_id"`
+	ReceiverID string             `json:"receiver_id" bson:"receiver_id"`
+	Content    string             `json:"content" bson:"content"`
+	Type       string             `json:"type" bson:"type"`
+	Status     string             `json:"status" bson:"status"`
+	CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
 }
